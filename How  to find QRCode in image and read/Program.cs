5ac7b9c3aaed7ff2a6902505a7c6e27923a80537
saman azadi <samanazadi1996@gmail.com>
2020-11-08 19:26:37 +0330

    Add project files.

---
+++ How  to find QRCode in image and read/Program.cs
@@ -0,0 +1,27 @@
+﻿using BC.NetBarcodeReaderTrial.All;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace How__to_find_QRCode_in_image_and_read
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var image = "C://versions-qr-codes.png";
+            ReadAllQRCodeTypefromImage(image);
+            Console.ReadKey();
+        }
+        public static void ReadAllQRCodeTypefromImage(string fileUrl)
+        {
+            string[] data = NetBarcodeReader.Recognize(fileUrl, NetBarcodeReader.Qrcode);
+            foreach (string result in data)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
